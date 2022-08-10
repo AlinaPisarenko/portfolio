c7d0e3629c5f5908b1bfc24a8d3709aa9a07139f
Alina <pisarenko.alina@gmail.com>
2022-08-10 00:06:44 -0400

    initialized navbar, 'about me' page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
 function About() {
-  return <div className='about'>About</div>;
+  return (
+    <div className='about'>
+      <h2 className='about__name heading-2'>About me</h2>
+      <p className='about__text'>
+        I’ve never thought I could be so enthusiastic about something. But then
+        I’ve tried coding. <br /> <br />
+        After getting my master’s degree in design and architecture at the State
+        Technical University of Saratov, I spent a few years working at a design
+        studio, but always had a dream to move to the United States. So one day
+        I got myself a one-way ticket and decided to change my life completely.
+        I moved to New York in 2016 and have been bartending since then while
+        trying to find my true passion. I've been interested in tech since my
+        college years, and computer science classes were always a highlight of
+        my week. So finally I started to code in the beginning of 2021 and I
+        can't stop since then.
+      </p>
+      <p className='about__text'>
+        I think of myself as a goal-oriented person, who is able to cultivate
+        new skills and is easy to work with, a team player, who can also work
+        independently. Studying and working in the architecture field helped me
+        become an organized and persistent worker with attention to detail,
+        while my bartending experience gave me the ability to work well under
+        pressure, multitask and be able to get along with new people easily.{' '}
+        <br /> <br />
+        Just like architecture, which is an art used by people daily, coding is
+        giving me an opportunity to create something people can interact, use
+        and work with on a daily basis. This is one of the main reasons why web
+        development became a part of my life.
+      </p>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import logoSm from '../images/logo-sm.svg';
 
 function Navbar() {
-  return <div className='navbar'></div>;
+  return (
+    <div className='navbar'>
+      <img className='logo' src={logoSm}></img>
+      <a href='#' className='navbar__link'>
+        Home
+      </a>
+      <a href='#' className='navbar__link'>
+        About me
+      </a>
+      <a href='#' className='navbar__link'>
+        My work
+      </a>
+      <a href='#' className='navbar__link'>
+        Contact me
+      </a>
+    </div>
+  );
 }
 
 export default Navbar;
