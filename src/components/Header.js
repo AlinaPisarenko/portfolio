4d29daaeb215c7a8cd910a5ea8f8332c3b1f838d
Alina <pisarenko.alina@gmail.com>
2022-08-09 22:40:52 -0400

    initialized header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className='header'>
+      <h2 className='header__greeting heading-3'>Hi, I am</h2>
+      <h1 className='header__name heading-1'>ALINA PISARENKO</h1>
+      <h2 className='heading-3'>
+        Web developer, Front end specialist based in New York.
+      </h2>
+      <div className='header__btn-group'>
+        <button className='btn'>My Work</button>
+        <button className='btn'>Contact me</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
